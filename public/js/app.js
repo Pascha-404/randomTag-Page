5adc06e64c402e1d42ccbb808f8f6cd6c25d2086
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-01-24 19:58:49 +0100

    added logic for random picker

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 const tagsContainer = document.querySelector('#tags');
 const textarea = document.querySelector('#textarea');
 
-//focus on page directly in textarea
+//focus on page directly on textarea
 textarea.focus();
 
 function createTags(input) {
@@ -21,7 +21,59 @@ function createTags(input) {
     })
 }
 
+//times it goes through tags + how fast
+function randomSelect() {
+    const times = 25;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unhighlightTag(randomTag)
+        }, 100);
+    }, 100);
+
+    //clears interval after choosen 'times' and sets final highlight
+    setTimeout(() => {
+        clearInterval(interval);
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag();
+            highlightTag(randomTag)
+        }, 100);
+
+    }, times * 100);
+}
+
+//picks a random tag
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+//adds highlight class
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+//removes highlight class 
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight');
+}
+
 // listens to keyup movement in textarea
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    //looks for 'Enter'-Key to start + clears textarea
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = '';
+        }, 10)
+
+        //starts random selector function
+        randomSelect()
+    }
 })
